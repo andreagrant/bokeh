16659afe3d62a54bba25e8fb9b1f2d18d0ab29e0
Andrea Grant <gran0260@umn.edu>
2016-04-02 12:34:39 -0500

    starting to add data import so I can add bar chart, eventually. Got no-state-selected logic working

--- mapPlot.py
+++ mapPlot.py
@@ -2,8 +2,10 @@
 
 from bokeh.plotting import figure
 from bokeh.sampledata.us_states import data as states
-from bokeh.models import HBox
+from bokeh.models import HBox, ColumnDataSource
 from bokeh.io import curdoc
+import pandas
+import os
 
 #del states["AK"]
 state_xs=[states[code]["lons"] for code in states]
@@ -14,16 +16,34 @@ stateNames=[states[code]["name"] for code in states]
 plotMap = figure(toolbar_location="right",plot_width=1100,plot_height=800,tools="tap")
 plotMap.patches(state_xs,state_ys,fill_alpha=0.5,line_color="black",line_width=2,line_alpha=0.3,name="states")
 
+#add a bar chart of the data
+
+#first, get the data
+#eventually, use the API to pull it from 
+#http://www.eia.gov/electricity/data.cfm#consumption
+#electricity generation by state and fuel type, monthly, for residential gen only
+myPath=os.path.dirname(os.path.abspath(__file__))
+rawData=pandas.read_csv(os.path.join(myPath,'Net_generation_for_electric_power.csv'),header=4)
+
+#create a column data source that will be updated in the on_selection_change :)
+#fuelData=ColumnDataSource()
+#create the bar chart
+plotFuel=figure(plot_width=400, plot_height=200)
+
 figs=HBox(children=[plotMap])
+#this below makes two copies othe map :(
 #curdoc().add_root(HBox(children=[figs],width=1200))
 
 #now, to figure out which state is selected
 #https://groups.google.com/a/continuum.io/forum/#!topic/bokeh/XGvStBxaz_M
 def on_selection_change(attr, old, new):
     thisInd=new['1d']['indices']
-    print thisInd
-    thisState=stateNames[thisInd[0]]
-    print thisState
+    if len(thisInd)>0:
+        print thisInd
+        thisState=stateNames[thisInd[0]]
+        print thisState
+    else:
+        print "all USA"
     
 renderer=plotMap.select(dict(name="states"))
 patches_ds=renderer[0].data_source
